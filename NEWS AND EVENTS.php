84078ae1aef81e567da7b5ca841b0d89359de7d4
GMC-Tech <gmdcinco@gmail.com>
2023-04-26 20:39:52 +0800

    News and Events

---
+++ NEWS AND EVENTS.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style >
+
+.button {
+  background-color: #033296; 
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+}
+
+.button1 {border-radius: 25px; 
+		  font-size: 30px;
+		  padding: 20px;
+		  width: 300px;
+		  margin: 4px 20px;}
+		  
+.button2 {border-radius: 25px; 
+		  font-size: 16px;
+		  padding: 10px;
+		  width: 120px;
+		  margin: 4px 5px;}		  
+
+.button1:hover {
+  background-color: #555555;
+  color: white;}
+  
+.button2:hover {
+  background-color: #555555;
+  color: white;}
+  
+p{
+font-size: 16px;
+width:80%;
+height: 150px; }
+
+img{
+padding: 10px 90px;
+float: left; }
+  
+</style>
+</head>
+<body>
+
+<br><br>
+
+
+<div style="text-align:center">
+<a href="" class="button button1"><b>News</b></a>
+<a href="" class="button button1"><b>Events</b></a>
+</div>
+
+<br><br>
+
+
+<h1 style="text-align:center">NEWS AND EVENTS</h1>
+
+<br>
+<br>
+<br>
+<div>
+
+
+<br><br>
+<br><br>
+
+</body>
+</html>
