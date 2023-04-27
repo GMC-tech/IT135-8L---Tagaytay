2681a012a09dab0a3a6f0510fc9304018e981aad
styoopid <annpatatas@gmail.com>
2023-04-27 20:12:43 +0800

    Fixed View User + Edit User

--- viewUser.php
+++ viewUser.php
@@ -7,38 +7,54 @@ $result = mysqli_query($conn, "SELECT * FROM `users` ORDER BY `user_id` DESC");
 ?>
 
 <html>
-<head>	
-	<title>Document</title>
+<head>
+	<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="viewUser.css">	
+	<title>Manage Users | Taytay Agriculture Office</title>
 </head>
 
 <body>
-	<h2>Add</h2>
-	<p>
-		<a href="addUser.php">Add New Data</a>
-	</p>
-	<table width='80%' border=0>
-		<tr>
-			<td><strong>First Name</strong></td>
-			<td><strong>Last Name</strong></td>
-            <td><strong>Username</strong></td>
-			<td><strong>Email</strong></td>
-            <td><strong>Password</strong></td>
-			<td><strong>Action</strong></td>
-		</tr>
-		<?php
+	<div class="view-user-heading">
+		<h1>MANAGE USERS</h1>
+    </div>
+	<div class="view-user-container">
+		<div class="view-user-table">
+			<table>
+				<tr>
+					<td><strong>First Name</strong></td>
+					<td><strong>Last Name</strong></td>
+					<td><strong>User Type</strong></td>
+					<td><strong>Username</strong></td>
+					<td><strong>Email</strong></td>
+					<td><strong>Password</strong></td>
+					<td><strong>Action</strong></td>
+				</tr>
+                
+			
+				<?php
 
-		while ($query = mysqli_fetch_assoc($result)) {
-			echo "<tr>";
-			echo "<td>".$query['first_name']."</td>";
-			echo "<td>".$query['last_name']."</td>";;	
-            echo "<td>".$query['username']."</td>";
-			echo "<td>".$query['email']."</td>";;	
-            echo "<td>".$query['password']."</td>";
-			echo "<td><a href=\"editUser.php?id=$query[user_id]\">Edit</a> | 
-			<a href=\"delete.php?id=$query[user_id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-		}
-		?>
-	</table>
+				while ($query = mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+					echo "<td>".$query['first_name']."</td>";
+					echo "<td>".$query['last_name']."</td>";
+					if($query['user_type']==0){
+						echo "<td>User</td>";
+					}else{
+						echo "<td>Admin</td>";
+					}	
+					echo "<td>".$query['username']."</td>";
+					echo "<td>".$query['email']."</td>";	
+					echo "<td>".$query['password']."</td>";
+					echo "<td><a href=\"editUser.php?id=$query[user_id]\">Edit</a> | 
+					<a href=\"delete.php?id=$query[user_id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+				}
+				?>
+			</table>
+		</div>
+	</div>
+	<br><br><br><br>
 </body>
 </html>
 <?php include "footer.html"; ?>
