ad6cfa4e7f4734fd2e79f52f5325207e41615ec8
Ninjatabbycatto <lb.leonbuena@gmail.com>
2023-04-27 22:58:29 +0800

    Margin + size fix + reposition iframe

--- contactUs.php
+++ contactUs.php
@@ -20,37 +20,29 @@ include "contact-form-handler.php";
 <body>
     <div class="content">
         
-        <div class="filler" style="height: 15vw;"></div>
+        <div class="filler" style="height: 11vw;"></div>
         
         <h1>CONTACT US</h1>
-        <div class="filler"></div>
-        <div class="filler"></div>
+</br>
         <a href="#">
         <div class="container1">
-            <i class="fa-solid fa-phone fa-2xl"></i>
+            <i class="fa-solid fa-phone fa-xl"></i>
             <h3>8XXX-XXXX</h3>
             <p>Vitae ut leo lorem in turpis </p>
         </div>
         </a>
         <a href="#">
         <div class="container2">
-            <i class="fa-sharp fa-solid fa-envelope fa-2xl"></i>
+            <i class="fa-sharp fa-solid fa-envelope fa-xl"></i>
             <h3>8XXX-XXXX</h3>
             <p>Vitae ut leo lorem in turpis </p>
         </div>
     </a>
         <div class="filler"></div>
         <div class="filler"></div>
-        <div class="filler"></div>
-        <div class="filler"></div>
-        <div class="filler"></div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d685.2294305271922!2d121.1332975871802!3d14.5582138783657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c71c6975e357%3A0xde76a7bf117d5749!2sTaytay%20Agricultural%20Office!5e0!3m2!1sen!2sph!4v1681814912964!5m2!1sen!2sph" 
-         allowfullscreen="true" name="Taytay Agriculture"
-        loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
-        <div class="filler"></div>
+        
         <h2>You may also contact us using this form</h2>
-        <div class="filler"></div>
+        <div class="filler" style="height: 1vw;"></div>
         <form action="contact-form-handler.php" name="contact" method="POST">
             <div class="half1">
                 <label for="fname">First Name</label>
@@ -60,7 +52,7 @@ include "contact-form-handler.php";
                 <label for="lname">Last Name</label>
                 <input type="text" id="lname" name="lastname"><br><br>
             </div>
-            <br><br><br><br><br>
+            <br><br><br>
             <label for="uemail">E-mail</label>
             <input type="email" id="uemail" name="uemail">
             <label for="subject">Subject</label>
@@ -71,7 +63,13 @@ include "contact-form-handler.php";
             <button type="submit" name="submit" value="Submit">Submit</button>
         </form>
 
-        <div class="filler"></div>
+        <div class="filler" style="height: 2vw;"></div>
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d685.2294305271922!2d121.1332975871802!3d14.5582138783657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c71c6975e357%3A0xde76a7bf117d5749!2sTaytay%20Agricultural%20Office!5e0!3m2!1sen!2sph!4v1681814912964!5m2!1sen!2sph" 
+             allowfullscreen="true" name="Taytay Agriculture"
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        </div>
         <div class="filler"></div>
     </div>
     
