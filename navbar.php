0668075a9ea3f96b69daca64ca967827c82afc13
GMC-Tech <gmdcinco@gmail.com>
2023-05-02 10:47:02 +0800

    Major Changes - UI, backend functionality, hosting, and DB changes

--- navbar.php
+++ navbar.php
@@ -35,14 +35,14 @@
                 <li><a href="user-home.php">HOME</a></li>
                 <li><a href="aboutUs.php">ABOUT US</a></li>
                 <li><a href="programs.php">PROGRAMS</a></li>
-                <li><a href="#"  style="text-decoration: none; cursor: default;">NEWS & EVENTS +</a>
+                <li><a>NEWS & EVENTS +</a>
                     <ul>
                         <li><a href="news-headline.php">NEWS</a></li>
                         <li><a href="event-headline.php">EVENTS</a></li>
                     </ul>
                 </li>
                 <li><a href="contactUs.php">CONTACT US</a></li>
-                <li><a href="#" style="text-decoration: none; cursor: default;">ACCOUNT</a>
+                <li><a href="#">ACCOUNT</a>
                     <?php if(empty($_SESSION['username']))
                             {
                                 echo '<ul><li><a href="logIn.php">LOGIN</a></li></ul>';
--- viewUser.php
+++ viewUser.php
@@ -8,59 +8,41 @@ $result = mysqli_query($conn, "SELECT * FROM `users` ORDER BY `user_id` DESC");
 
 <html>
 <head>
-	<meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="viewUser.css">	
-	<title>Manage Users | Taytay Agriculture Office</title>
+	<link rel="stylesheet" type="text/css" href="viewUser.css">
+	<title>Document</title>
 </head>
 
 <body>
+<div class="table-container">
+	<h2>View Users</h2>
+	<a class="add-link" href="addUser.php">Add New Data</a>
+	<table>
+		<thead>
+		<tr>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Username</th>
+			<th>Email</th>
+			<th>Password</th>
+			<th>Action</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
 
-	<div class="start"></div>
-	
-	<div class="view-user-heading">
-		<h1>MANAGE USERS</h1>
-
-    </div>
-	<div class="view-user-container">
-		<div class="view-user-table">
-			<table>
-				<tr>
-					<td><strong>First Name</strong></td>
-					<td><strong>Last Name</strong></td>
-					<td><strong>User Type</strong></td>
-					<td><strong>Username</strong></td>
-					<td><strong>Email</strong></td>
-					<td><strong>Password</strong></td>
-					<td><strong>Action</strong></td>
-				</tr>
-                
-			
-				<?php
-
-				while ($query = mysqli_fetch_assoc($result)) {
-					echo "<tr>";
-					echo "<td>".$query['first_name']."</td>";
-					echo "<td>".$query['last_name']."</td>";
-					if($query['user_type']==0){
-						echo "<td>User</td>";
-					}else{
-						echo "<td>Admin</td>";
-					}	
-					echo "<td>".$query['username']."</td>";
-					echo "<td>".$query['email']."</td>";	
-					echo "<td>".$query['password']."</td>";
-					echo "<td><a href=\"editUser.php?id=$query[user_id]\">Edit</a> | 
-					<a href=\"delete.php?id=$query[user_id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-				}
-				?>
-			</table>
-		</div>
-	</div>
-
-	<div class="end"></div>
-
+		while ($query = mysqli_fetch_assoc($result)) {
+			echo "<tr>";
+			echo "<td>".$query['first_name']."</td>";
+			echo "<td>".$query['last_name']."</td>";;	
+            echo "<td>".$query['username']."</td>";
+			echo "<td>".$query['email']."</td>";;	
+            echo "<td>".$query['password']."</td>";
+			echo "<td><a href=\"editUser.php?id=$query[user_id]\">Edit</a> | 
+			<a href=\"delete.php?id=$query[user_id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+		}
+		?>
+		</tbody>
+	</table>
 </body>
 </html>
-<?php include "admin-footer.html"; ?>
+
